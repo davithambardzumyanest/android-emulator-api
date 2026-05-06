408a44915bd6b284742eb5a3d2efc89fab3717f8
Davit <davit.hambardzumyan.est@gmail.com>
2026-05-06 16:31:05 +0400

    add install sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,15 +2,15 @@
 set -euo pipefail
 
 AVD_NAME="${DEFAULT_AVD_NAME:-pixel3_api33_1}"
-AVD_PACKAGE="${DEFAULT_AVD_PACKAGE:-system-images;android-30;google_apis;x86_64}"
+AVD_PACKAGE="${DEFAULT_AVD_PACKAGE:-system-images;android-33;google_apis;x86_64}"
 
 mkdir -p /root/.android/avd
 touch /root/.android/repositories.cfg || true
 
 if command -v avdmanager >/dev/null 2>&1; then
-  if [ ! -f "/root/.android/avd/pixel3_api33_1.ini" ]; then
-    echo "[entrypoint] AVD 'pixel3_api33_1' missing; creating..."
-    echo "no" | avdmanager create avd -n "pixel3_api33_1" -k "${AVD_PACKAGE}" --force >/dev/null
+  if [ ! -f "/root/.android/avd/${AVD_NAME}.ini" ]; then
+    echo "[entrypoint] AVD '${AVD_NAME}' missing; creating..."
+    echo "no" | avdmanager create avd -n "${AVD_NAME}" -k "${AVD_PACKAGE}" --force >/dev/null
   fi
 fi
 
